3e1541282f854304537767055567c702cfb4a43b
Lawrenkid <atlasjust1@gmail.com>
2022-10-19 16:32:56 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/**
- * main - Print _putchar
+/*
+ * main - prints putchar
  * Return: Always 0
  */
 int main(void)
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
